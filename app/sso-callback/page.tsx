a71db290e87bfce9a2d44b7eb6efb47ba55f8799
Rodney Urquhart <rodneyu85@gmail.com>
2024-03-01 01:33:44 -0800

    Updating SSO callback

--- app/sso-callback/page.tsx
+++ app/sso-callback/page.tsx
@@ -1,19 +1,10 @@
-import { type HandleOAuthCallbackParams } from '@clerk/types';
-import SSOCallback from '@/components/sso-callback';
+import { AuthenticateWithRedirectCallback } from '@clerk/nextjs';
 import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'SSO-Callback | Platforms Starter Kit',
 };
 
-export interface SSOCallbackPageProps {
-  searchParams: HandleOAuthCallbackParams;
-}
-
-export default function SSOCallbackPage({
-  searchParams,
-}: SSOCallbackPageProps) {
-  debugger;
-  console.log(searchParams);
-  return <SSOCallback searchParams={searchParams} />;
+export default function SSOCallbackPage() {
+  return <AuthenticateWithRedirectCallback />;
 }
